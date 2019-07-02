0a9196c487b90093c358eaf76d5aa8be8f412e91
Pabitra Mohan Swain <mohan.leed07@gmail.com>
2019-07-03 02:34:32 +0530

    changed

--- src/main/java/com/sb/service/ProductService.java
+++ src/main/java/com/sb/service/ProductService.java
@@ -4,6 +4,6 @@ public class ProductService {
 
 	public static void main(String[] args) {
 		System.out.println("Service wil get data from dao classes");
-		System.out.println("End");
+		System.out.println("thssns smm,amhss");
 	}
 }
